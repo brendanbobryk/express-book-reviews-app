bed5e5db9fdd9a0716c19c1deaba6a8fdc3e0a3d
Brendan Bobryk <babobryk@gmail.com>
2026-01-16 12:01:34 -0800

    completed the code for getting the book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,8 +16,15 @@ public_users.get('/', function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn; // retrieve ISBN from route
+    const book = books[isbn];     // find the book by ISBN
+
+    if (book) {
+        return res.send(JSON.stringify(book, null, 4));
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 // Get book details based on author
